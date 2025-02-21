79fe7804dd428fb7e07dd1102c426c7eefc2fa00
theo-ack <theo4701@gmail.com>
2025-02-21 09:36:18 +0100

    /zoo

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(cors());
 app.route("/compounds", compoundRouter);
 app.route("/animals", animalRouter);
 app.route("/staff", staffRouter);
-app.route("zoo", zooRouter);
+app.route("/zoo", zooRouter);
 
 app.get("/", async (c) => {
   return c.text("Hello Theo! 🔥");
